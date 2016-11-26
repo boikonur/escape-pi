6d6654249d9d0b5a6ae9cd6767938518e55fd7ff
Boyko Kazakov <boikonur@gmail.com>
2016-11-26 15:19:15 +0200

    fix bg color

--- main_task.py
+++ main_task.py
@@ -79,7 +79,7 @@ class Hell_Player():
             l2w, l2h = label2.get_size()
             # Clear screen and draw text with line1 above line2 and all
             # centered horizontally and vertically.
-            self._screen.fill(self._bgcolor)
+            self._screen.fill(BLACK)
             self._screen.blit(label1, (sw/2-l1w/2, sh/2-l2h/2-l1h))
             self._screen.blit(label2, (sw/2-l2w/2, sh/2-l2h/2))
             pygame.display.update()
